052221f0aade96c67cd1c2cde491fb031b97800b
Oleksandr Fedotov <fedotov4815162342@gmail.com>
2019-01-20 13:27:44 +0100

    implement todo app, add vue-router, add axios

---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Example todo app</h1>
+        <p>This app is built based on tutorial video.</p>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'About'
+}
+</script>
+
+<style scoped>
+</style>
+
